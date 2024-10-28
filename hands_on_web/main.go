0edf9136db54ea1374042a8b163acae49034dda7
taisei <hutoboshi.eng@gmail.com>
2024-10-28 19:20:30 +0900

    ポート番号カーソル指定

--- hands_on_web/main.go
+++ hands_on_web/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
@@ -11,19 +12,23 @@ import (
 )
 
 func main() {
-	err := http.ListenAndServe(":18080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-	}))
-
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
 	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
+		log.Fatalf("failed to listen port %v", err)
+	}
+	if err := run(context.Background(), l); err != nil {
+		log.Printf("failed to close: %v", err)
 		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
@@ -31,8 +36,8 @@ func run(ctx context.Context) error {
 	eg, ctx := errgroup.WithContext(ctx)
 
 	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
+		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %v", err)
 			return err
 		}
 		return nil
